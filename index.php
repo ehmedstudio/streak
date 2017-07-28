4d2cbfa0b7dbca113f177d7b9dee2e3094b1dea3
Ehmed <ehmedstudio@gmail.com>
2017-07-28 10:36:47 +0000

    Created the basic HTML template for the app

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Streak | The Habit Builder</title>
+	
+	<link rel="stylesheet" href="src/css/main.css" type="text/css" />
+</head>
+<body>
+	<!-- CONTAINER -->
+    <div id="container">
+      <!-- HEADER -->
+        <header id="header" class="master">
+          <!-- LOGO -->
+            <span id="logo" class="highlight">
+              <h1><a href="index.php">Streak</a></h1>
+            </span>
+          <!-- /LOGO -->
+        </header>
+      <!-- /HEADER -->
+    </div>
+	<!-- /CONTAINER -->
+</body>
+</html>
